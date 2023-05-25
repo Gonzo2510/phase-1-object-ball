26ebb36d7042eaeea52e9523cca7fde8404934e7
Gonzo2510 <52935700+Gonzo2510@users.noreply.github.com>
2023-05-25 08:44:17 -0500

    Update 00-objectball.js
    begin gameObject

---
+++ src/00-objectball.js
@@ -0,0 +1,14 @@
+function gameObject() {
+    const object = {
+        home : {
+            teamName : 'Brooklyn Nets'
+            colors : [Black, White]
+            players : ''
+        }
+        away : {
+            teamName : ''
+            colors : []
+            players : ''
+        }
+    }
+};
